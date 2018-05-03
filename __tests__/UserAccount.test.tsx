9013f559572b580cefc73462e244745a42b7ec2b
Adam Burdette <alburdet619@gmail.com>
2018-05-03 11:46:16 -0400

    UserAccount tests
    Since this screen is not using any reusable components, it should have a snapshot.

---
+++ __tests__/UserAccount.test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import UserAccount from '../src/screens/UserAccount';
+import * as mocks from './__mocks__/mocks';
+
+describe('Tests the states of UserAccount', () => {
+  it('renders without crashing with an image source', () => {
+    const nav = {
+      ...mocks.nav,
+      state: {
+        params: { source: mocks.source, username: 'TEST', reload: jest.fn() },
+      },
+    };
+
+    const rendered = renderer.create(<UserAccount navigation={nav} />).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it('renders without crashing with the default icon source', () => {
+    const nav = {
+      ...mocks.nav,
+      state: {
+        params: {
+          source: 'account-outline',
+          username: 'TEST',
+          reload: jest.fn(),
+        },
+      },
+    };
+    const rendered = renderer.create(<UserAccount navigation={nav} />).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+});
