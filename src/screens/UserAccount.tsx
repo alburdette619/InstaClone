79a6fdb5a161517fefb12e75601c68b380c73179
Adam Burdette <alburdet619@gmail.com>
2018-05-03 11:36:03 -0400

    User Account screen
    The user account screen now shows a large user avatar that when pressed, allows the user to set their avatar and a text input that allows the user to adjust their username.  When either of these are set, the parent UserImages view is reloaded with data and the items are saved to disk.

--- src/screens/UserAccount.tsx
+++ src/screens/UserAccount.tsx
@@ -1,19 +1,99 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  ImageURISource,
+  AsyncStorage,
+} from 'react-native';
+import { Avatar } from 'react-native-elements';
+import CommonProps, { SingleImageProps } from '../types/interfaces';
+import addImage from '../utils/addImage';
+import getAvatarSourceProp from '../utils/getAvatarSource';
 
-export interface UserAccountProps {}
-interface UserAccountState {}
+interface UserAccountState {
+  currentUsername: string;
+  currentAvatar: ImageURISource | string;
+}
 
-class UserAccount extends Component {
+class UserAccount extends Component<CommonProps, UserAccountState> {
+  constructor(props: CommonProps) {
+    super(props);
+
+    const { username, source } = this.props.navigation.state.params;
+    this.state = {
+      currentUsername: username === 'New User' ? '' : username,
+      currentAvatar: source,
+    };
+  }
+
   static navigationOptions = {
     title: 'Account',
   };
 
-  state = {};
+  setAvatar(uri: string) {
+    AsyncStorage.setItem('avatarSource', uri, () => {
+      this.setState({ currentAvatar: { uri } });
+      const { reload } = this.props.navigation.state.params;
+      if (reload) {
+        reload();
+      }
+    });
+  }
+
+  setUsername(username: string) {
+    AsyncStorage.setItem('username', username, () => {
+      const { reload } = this.props.navigation.state.params;
+      if (reload) {
+        reload();
+      }
+    });
+  }
+
   render() {
+    const { currentUsername, currentAvatar } = this.state;
+
     return (
-      <View style={{ flex: 1 }}>
-        <Text>User Account</Text>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+        }}
+      >
+        <KeyboardAvoidingView
+          behavior="position"
+          style={{
+            flex: 1,
+            justifyContent: 'space-between',
+            marginHorizontal: 8,
+            marginVertical: 16,
+          }}
+        >
+          <Avatar
+            height={360}
+            rounded
+            containerStyle={{ alignSelf: 'center' }}
+            onPress={() =>
+              addImage((uri: string) => {
+                this.setAvatar(uri);
+              })
+            }
+            {...getAvatarSourceProp(currentAvatar, 320) as any}
+          />
+          <TextInput
+            value={currentUsername}
+            autoCorrect={false}
+            placeholder="Username"
+            returnKeyType="done"
+            style={{
+              height: 60,
+              fontSize: 28,
+              marginTop: 32,
+            }}
+            onChangeText={text => this.setState({ currentUsername: text })}
+            onEndEditing={event => this.setUsername(event.nativeEvent.text)}
+          />
+        </KeyboardAvoidingView>
       </View>
     );
   }
